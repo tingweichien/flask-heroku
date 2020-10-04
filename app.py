3b66c161f813233103e0fdc3b9d3ef689b987d32
timwei <tim960622@gmail.com>
2020-10-05 02:05:51 +0800

    add the templates and static file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # __name__ represent the current module
 app = Flask(__name__)
@@ -6,12 +6,22 @@ app = Flask(__name__)
 # Decorator 函式的裝飾:以函式為基礎,提供附加功能
 @app.route("/")
 def home():
-    return "This is Ting Wei Chein first flask web application deploy"
+    return render_template("home.html")
 
 @app.route("/test") # 代表我們要處理的路徑
 def text():
     return "This is test"
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+
 if __name__ == "__main__":
     #啟動伺服器
-    app.run()
+    app.run(debug=True)
