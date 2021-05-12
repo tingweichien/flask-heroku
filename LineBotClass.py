b79fd543d431ed233f40a1c2ed56c5605010bd03
timwei <tim960622@gmail.com>
2021-05-13 01:55:50 +0800

    [2021/05/13] Modify  the function for login and remove the useless class

--- LineBotClass.py
+++ LineBotClass.py
@@ -16,84 +16,80 @@ gHandler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 #\ text for login
 gEventText = ""
 
-
 #\ message text
 gIsJustText = True
 
-class LineBotClass():
-    #\ -- CONSTRUCTOR --
-    def __init__(self, app:Flask):
-        self.app = app
-    #\ -- METHOD --
-
-    #\ handler. This is the example from the official doc
-    def LineBotHandler(self):
-        #\ get X-Line-Signature header value
-        #\ 如果該HTTP POST訊息是來自LINE平台，在HTTP請求標頭中一定會包括X-Line-Signature項目，
-        #\ 該項目的內容值是即為數位簽章。例如：
-        signature = request.headers['X-Line-Signature']
-
-        #\ get request body as text
-        body = request.get_data(as_text=True)
-        self.app.logger.info("Request body: " + body)
-        print(body)
-
-        #\ handle webhook body
-        try:
-            gHandler.handle(body, signature)
-        except InvalidSignatureError:
-            print("Invalid signature. Please check your channel access token/channel secret.")
-            abort(400)
-
-        return 'OK'
 
 
 
-    #\ handle the message
-    @gHandler.add(MessageEvent, message=TextMessage)
-    def handle_text_message(event):
-        global gIsJustText
-        print("[INFO]: TextMessage")
-        print(f"[INFO]: {event}")
-        if event.source.user_id != "U00a49f1618f9827d4b24f140c2e5f770":
+#\ handler. This is the example from the official doc
+def LineBotHandler(app):
+    #\ get X-Line-Signature header value
+    #\ 如果該HTTP POST訊息是來自LINE平台，在HTTP請求標頭中一定會包括X-Line-Signature項目，
+    #\ 該項目的內容值是即為數位簽章。例如：
+    signature = request.headers['X-Line-Signature']
 
-            #\ switch the case of MessageEvent
-            if gIsJustText == True :
-                gEventText = event.message.text.lower()
+    #\ get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    print(body)
 
-            if gEventText == "login":
-                gLine_bot_api.reply_message(event.reply_token, TextSendMessage(text=Index.LoginEventText[0]))
-                Index.LoginData["Account"] = event.message.text
-                gLine_bot_api.reply_message(event.reply_token, TextSendMessage(text=Index.LoginEventText[1]))
-                Index.LoginData["Password"] = event.message.text
-                self.Login2Web(event)
+    #\ handle webhook body
+    try:
+        gHandler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
 
-            elif gEventText == "menu":
-                pass
-            else :
-                gLine_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
-
-            #\ reset the is just text flag
-            gIsJustText = True
+    return 'OK'
 
 
-    #\ for the first time joining the group
-    @gHandler.add(JoinEvent)
-    def handle_message(event):
-        global gEventText, gIsJustText
-        gEventText = "login"
-        gIsJustText = False
-        print("[INFO]: TextMessage")
-        print(f"[INFO]: {event}")
-        for idx in range(Index.JointTextFirstPointer):
-            gLine_bot_api.reply_message(
-                            event.reply_token,
-                            TextSendMessage(text=Index.JoinEventText[idx])
-                            )
+
+#\ handle the message
+@gHandler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    global gIsJustText
+    print("[INFO]: TextMessage")
+    print(f"[INFO]: {event}")
+    if event.source.user_id != "U00a49f1618f9827d4b24f140c2e5f770":
+
+        #\ switch the case of MessageEvent
+        if gIsJustText == True :
+            gEventText = event.message.text.lower()
+
+        if gEventText == "login":
+            gLine_bot_api.reply_message(event.reply_token, TextSendMessage(text=Index.LoginEventText[0]))
+            Index.LoginData["Account"] = event.message.text
+            gLine_bot_api.reply_message(event.reply_token, TextSendMessage(text=Index.LoginEventText[1]))
+            Index.LoginData["Password"] = event.message.text
+            Login2Web(event)
+
+        elif gEventText == "menu":
+            pass
+        else :
+            gLine_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+
+        #\ reset the is just text flag
+        gIsJustText = True
 
 
-    #\
-    def Login2Web(self, event):
+#\ for the first time joining the group
+@gHandler.add(JoinEvent)
+def handle_message(event):
+    global gEventText, gIsJustText
+    gEventText = "login"
+    gIsJustText = False
+    print("[INFO]: TextMessage")
+    print(f"[INFO]: {event}")
+    for idx in range(len(Index.JoinEventText)):
         gLine_bot_api.reply_message(
-                            event.reply_token,
-                            TextSendMessage(text=f'Hi, Check again again for the login info:\nAccount: {Index.LoginData["Account"]}\nPassword: {Index.LoginData["Password"]}'))
+                        event.reply_token,
+                        TextSendMessage(text=Index.JoinEventText[idx])
+                        )
+
+
+#\
+def Login2Web(event):
+    gLine_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=f'Hi, Check again again for the login info:\nAccount: {Index.LoginData["Account"]}\nPassword: {Index.LoginData["Password"]}'))
