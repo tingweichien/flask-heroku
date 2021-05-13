1a7cd2c7b4e4e9c8e67b8e8ab419c29f29f001db
timwei <tim960622@gmail.com>
2021-05-13 22:40:54 +0800

    [2021/05/13] Try to fix the import module error for index.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template, request, url_for, redirect, session
 from datetime import timedelta
-import Index
+import index
 import LineBotClass
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
@@ -51,7 +51,7 @@ def contact():
 
 @app.route("/OSMmap")
 def OSMmap():
-    return render_template("OSMmap.html", apikey = Index.GMAPapikey, api_on = Index.bAPIon)
+    return render_template("OSMmap.html", apikey = index.GMAPapikey, api_on = index.bAPIon)
 
 
 #\ -- to HTTP method --
