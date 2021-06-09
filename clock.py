8b812c5ca16ed91ad8f876701a4c1ee220f1db49
timwei <tim960622@gmail.com>
2021-06-10 02:33:50 +0800

    [2021/06/10] Finally build up the schedule to update the database

--- clock.py
+++ clock.py
@@ -22,24 +22,22 @@ def testing():
 
 #\ Set the timer to update the datebase
 #\ this will be trigger every
-@sched.scheduled_job('cron', hour=2, minute=30, second=0)
+@sched.scheduled_job('cron', hour=0, minute=0, second=0)
 def SetTimer2Update_job():
     global sched
     print(f"[INFO] SetTimer2Update_job start: {datetime.datetime.now().strftime('%Y-%m-%d, %H:%M:%S')}")
-    index.DAYAlarm["hour"] = 2 #\ we set the hour at 0 and let the minute and second to be random
+    # index.DAYAlarm["hour"] = 2 #\ we set the hour at 0 and let the minute and second to be random
 
     #\ Start from 1 min is because this function will be triggerred at 00:00:00, avoid conflict with this function with UpdateDataBase_job()
     #\ The reason to set the minutes boundary to 30 is due to the heroku free dyno will sleep every 30 minutes idling.
-    index.DAYAlarm["minute"] = 31#random.randint(1, 30)
-    index.DAYAlarm["second"] = 0#random.randint(0, 60)
+    index.DAYAlarm["minute"] = random.randint(1, 30)
+    index.DAYAlarm["second"] = random.randint(0, 60)
     # cache.set("DAYAlarm", index.HOURAlarm)
     sched.reschedule_job("UpdateDataBase_job_ID", trigger='cron', **index.DAYAlarm)
     print(f"[INFO] In SetTimer2Update_job() set the timer to update : {index.DAYAlarm}")
 
 
 #\ This is to update the database's TodayID eveyday midnight
-# @sched.scheduled_job('cron', **cache.get("DAYAlarm"))
-# @sched.scheduled_job('cron', id="UpdateDataBase_job_ID")
 def UpdateDataBase_job():
     print(f"[INFO] In UpdateDataBase_job() Update the database latest ID at {datetime.datetime.now().strftime('%Y-%m-%d, %H:%M:%S')}")
 
@@ -62,10 +60,9 @@ def UpdateDataBase_job():
 
 
 
-#\ use this function to start the alarm
-# def StartAlarm():
-#     sched.start()
+################################################################################################
 
+#\ Add the job
 sched.add_job(UpdateDataBase_job, "cron", id="UpdateDataBase_job_ID", hour=0, minute=1, second=0)
 
 #\ start the clock
