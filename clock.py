84eb244d06e013f74cfe9e7b3c515cc576c9d763
timwei <tim960622@gmail.com>
2021-06-09 22:10:50 +0800

    [2021/06/09] Test the schedule function

--- clock.py
+++ clock.py
@@ -22,18 +22,18 @@ def testing():
 
 #\ Set the timer to update the datebase
 #\ this will be trigger every
-@sched.scheduled_job('cron', hour=21, minute=59, second=0)
+@sched.scheduled_job('cron', hour=22, minute=15, second=0)
 def SetTimer2Update_job():
     print(f"[INFO] SetTimer2Update_job start: {datetime.datetime.now().strftime('%Y-%m-%d, %H:%M:%S')}")
     index.DAYAlarm["hour"] = 22 #\ we set the hour at 0 and let the minute and second to be random
 
     #\ Start from 1 min is because this function will be triggerred at 00:00:00, avoid conflict with this function with UpdateDataBase_job()
     #\ The reason to set the minutes boundary to 30 is due to the heroku free dyno will sleep every 30 minutes idling.
-    index.DAYAlarm["minute"] = 0#random.randint(1, 30)
+    index.DAYAlarm["minute"] = 16#random.randint(1, 30)
     index.DAYAlarm["second"] = 0#random.randint(0, 60)
     # cache.set("DAYAlarm", index.HOURAlarm)
     sched.reschedule_job("UpdateDataBase_job_ID", trigger='cron', **index.HOURAlarm)
-    print(f"[INFO] In SetTimer2Update_job() set the timer to update : {cache.get('DAYAlarm')}")
+    # print(f"[INFO] In SetTimer2Update_job() set the timer to update : {cache.get('DAYAlarm')}")
 
 
 #\ This is to update the database's TodayID eveyday midnight
